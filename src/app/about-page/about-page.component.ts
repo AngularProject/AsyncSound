f8cc5ceb6d635a970bed2dc6b7a3a4ef77f435eb
TeodSt <teod_stoyanova@abv.bg>
2016-12-22 23:28:51 +0200

    Changed href to routerLink

--- src/app/about-page/about-page.component.ts
+++ src/app/about-page/about-page.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.css']
 })
