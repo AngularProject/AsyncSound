edb35f9ae67eb724d99256fe3e18b26c83f6f392
Galvaneyes <m.v.manchev@gmail.com>
2016-12-29 18:00:42 +0200

    Fix typo

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { LoginPageComponent } from './login-page.component';
+import { LoginPageComponent } from './login.component';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
