df6805e7c28767333e8c3d8dc4702ce290c96b92
TeodSt <teod_stoyanova@abv.bg>
2016-12-31 01:24:13 +0200

    Set role done done

--- src/app/admin-page/admin-page.component.ts
+++ src/app/admin-page/admin-page.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AdminService} from '../../services/admin.service';
+import { NotificationsService } from '../../../node_modules/angular2-notifications';
+
+
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
   styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent implements OnInit {
+  user: string;
 
-  constructor() { }
+  constructor(private adminService: AdminService, private notification: NotificationsService) { }
 
   ngOnInit() {
   }
 
+  setRole() {
+    console.log(this.user);
+
+    this.adminService.setUserAsAdmin({'username': this.user})
+      .subscribe((response: any) => {
+          if (response.error) {
+              this.notification.error('Login failed!', response.message);
+            } else {
+              this.notification.success('Successfully added role to user ' + this.user, 'Welcome');
+              // setTimeout(() => this.router.navigateByUrl('/home'), 1500);
+            }
+        }, () => this.notification.error('Login failed!', 'Please try again.'));
+  }
 }
