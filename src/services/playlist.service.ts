ed1c3d3e8ad7365c1ffe8281a6346a63c890e922
Galvaneyes <m.v.manchev@gmail.com>
2017-01-02 01:37:23 +0200

    Added getUserPlaylist & addSongInPlaylist services

--- src/services/playlist.service.ts
+++ src/services/playlist.service.ts
@@ -1,12 +1,42 @@
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/map';
+
+import { HttpOptionsService } from './http.options.service';
 
 import { Playlist } from '../app/models/Playlist';
 import { Comment } from '../app/models/Comment';
+
+const PLAYLIST_URL = 'http://localhost:3000/api/playlists/';
 
 @Injectable()
 export class PlaylistService {
 	playlists: Playlist[] = [];
 
+	constructor(private http: Http, private httpOptionsService: HttpOptionsService) {
+    }
+
+    public getUserPlaylists(username): Observable<any> {
+
+		let url = PLAYLIST_URL + username;
+        let options: RequestOptions = this.httpOptionsService.getRequestOptions(false);
+
+        return this.http
+            .get(url, options)
+            .map((res: Response) => res.json());
+    }	
+
+	public addSongInPlaylist(data: Object): Observable<any> {
+
+        let body: string = JSON.stringify(data);
+        let options: RequestOptions = this.httpOptionsService.getRequestOptions(true);
+
+        return this.http
+            .post(PLAYLIST_URL, body, options)
+            .map((res: Response) => res.json());
+    }
 	getAllPlaylists() {
 		return this.playlists;
 	}
