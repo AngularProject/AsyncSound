c7dc6838f000e5575b407d1dcc2adb35516b12d1
Galvaneyes <m.v.manchev@gmail.com>
2017-01-02 22:00:58 +0200

    Export service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,3 @@
-import { SongService } from './songs.service';
 export { HttpOptionsService } from './http.options.service';
 export { LoginService } from './login.service';
 export { UserService } from './user.service';
@@ -7,3 +6,5 @@ export { PlaylistService } from './playlist.service';
 export { AdminService } from './admin.service';
 export { ProfileService } from './profile.service';
 export { SongService} from './songs.service';
+export { PageService } from './page.service';
+
