e2bd931167f7db1e0eb20b4fa7e276639cc6dea5
Galvaneyes <m.v.manchev@gmail.com>
2017-01-02 22:00:42 +0200

    Added methods

--- src/app/song/song.component.ts
+++ src/app/song/song.component.ts
@@ -1,34 +1,82 @@
 import { Component, OnInit, Output } from '@angular/core';
 
 import { Song } from '../models/Song';
-import { SongService } from '../../services';
+import { SongService, PageService } from '../../services';
 import { NotificationsService } from '../../../node_modules/angular2-notifications';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-song',
   templateUrl: './song.component.html',
   styleUrls: ['./song.component.css']
 })
+
 export class SongComponent implements OnInit {
   @Output() song;
+
   songs: Song[];
-  constructor(private songService: SongService, private notification: NotificationsService) { }
+  pageInfo: any = {};
+  pagedItems: any[];
+  searchingSong: string;
+  constructor(private songService: SongService,
+              private notification: NotificationsService,
+              private pageService: PageService,
+              private activatedRoute: ActivatedRoute,
+              private router: Router) {
+   }
 
   ngOnInit() {
-    this.getAllSongs();
+    this.activatedRoute.params
+        .map(query=> query['title'])
+        .subscribe(title=>{
+            if(!!title) {
+              this.getSongByTitle(title)
+            } else {
+              this.getAllSongs();
+            }
+        });
   }
 
-  getAllSongs() {
+  public getSongByTitle(title) {
+     this.songService
+         .getSongByTitle(title)
+         .subscribe(response => {
+
+            if(response.error) {
+              return;
+            } else {
+              this.notification.success('Songs loaded', ':)');
+              this.songs = response as Song[];
+              this.setPage(1);
+            };
+        }, ()=> this.notification.error('Loading songs failed', 'Error'))
+  }
+
+  public getAllSongs() {
     this.songService
         .getAllSongs()
         .subscribe( response => {
             if(response.error) {
               this.notification.error('Loading songs failed', response.message);
             } else {
-              this.notification.success('Songs loaded', ':)');
               this.songs = response as Song[];
+              this.setPage(1);
             };
         }, ()=> this.notification.error('Loading songs failed', 'Error'))
   }
 
-}
+  public search() {
+    let url = '/songs/' + this.searchingSong;
+    this.searchingSong = '';
+
+    this.router.navigateByUrl(url);
+  }
+  public setPage(page: number) {
+      if (page < 1 || page > this.pageInfo.totalPages) {
+            return;
+        }
+
+        this.pageInfo = this.pageService.getPage(this.songs.length, page);
+        this.pagedItems = this.songs.slice(this.pageInfo.startIndex, this.pageInfo.endIndex + 1);
+    }
+  }
